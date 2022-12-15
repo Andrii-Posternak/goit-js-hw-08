b2774eed3cb87a4fc7035da05197d2d7bac4508c
Andrii-Posternak <panposternak@gmail.com>
2022-12-15 23:58:47 +0200

    add task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+const throttle = require('lodash.throttle');
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem(STORAGE_KEY, seconds);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+player.setCurrentTime(currentTime);
